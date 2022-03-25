ccce980c34f35df62cc00c44e5412afcc8a928cc
Kevin Sauvage <kevin@subforce.io>
2022-03-25 18:02:21 +0100

    fix about section

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -18,10 +18,10 @@ const About = () => {
             (ES5, ES6), React.js (Hooks, react-router, context api) and Next.js, but I also enjoy
             the back-end using Node.js and Express.js. <br />
             <br />
-            To describe me, I would say that I'm passionate about what you do, naturally curious,
-            autonomous, committed, enthusiastic with a "can-do" attitude and I'm always enthusiastic
-            about learning new things. My past experience in the commerce industry brings me
-            powerful communication skills, and the ability to create strong working relationships.
+            To describe me, I would say that I'm passionate about what I do, naturally curious,
+            autonomous, committed, with a "can-do" attitude and always enthusiastic about learning
+            new things. My past experience in the commerce industry brings me powerful communication
+            skills, and the ability to create strong working relationships.
             <br />
             <br />
             When I'm not in front of the computer developing projects, I enjoy spending time in
