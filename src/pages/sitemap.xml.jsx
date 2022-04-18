9cfad0bf86fcac283e666a60baa780886eb60f70
Kevin Sauvage <kevinsauvage@outlook.com>
2022-04-18 16:30:12 +0200

    update sitemap

--- src/pages/sitemap.xml.jsx
+++ src/pages/sitemap.xml.jsx
@@ -1,9 +1,13 @@
+import projects from "../data/projects";
+
 const Sitemap = () => null;
 
 export const getServerSideProps = async ({ res, locales }) => {
   const BASE_URL = "https://www.kevin-sauvage.com/";
 
   const paths = [BASE_URL];
+
+  const projectPath = projects.map((item) => BASE_URL + `project/${item.title}`);
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -18,6 +22,17 @@ export const getServerSideProps = async ({ res, locales }) => {
           `;
         })
         .join("")}
+        ${projectPath
+          .map((url) => {
+            return `
+            <url>
+              <loc>${url}</loc>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+          })
+          .join("")}
     </urlset>
   `;
 
