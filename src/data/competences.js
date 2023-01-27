929945a55696ead77491b2c8ca4be890507477ff
Kevin Sauvage <kevinsauvage@outlook.com>
2023-01-28 00:05:17 +0100

    fix

--- src/data/competences.js
+++ src/data/competences.js
@@ -1,40 +1,14 @@
 const competences = [
-  {
-    icon: "devicon-html5-plain",
-    description: "HTML",
-  },
-  {
-    icon: "devicon-css3-plain",
-    description: "CSS",
-  },
-  {
-    icon: "devicon-sass-original",
-    description: "SASS",
-  },
-  {
-    icon: "devicon-javascript-plain",
-    description: "JavaScript",
-  },
-  {
-    icon: "devicon-react-original",
-    description: "React.js",
-  },
-  {
-    icon: "devicon-nextjs-original",
-    description: "Next.js",
-  },
-  {
-    icon: "devicon-nodejs-plain",
-    description: "Node.js",
-  },
-  {
-    icon: "devicon-express-original",
-    description: "Express.js",
-  },
-  {
-    icon: "devicon-firebase-plain",
-    description: "Firebase",
-  },
+  { icon: "devicon-html5-plain", description: "HTML" },
+  { icon: "devicon-css3-plain", description: "CSS" },
+  { icon: "devicon-sass-original", description: "SASS" },
+  { icon: "devicon-javascript-plain", description: "JavaScript" },
+  { icon: "devicon-react-original", description: "React.js" },
+  { icon: "devicon-nextjs-original", description: "Next.js" },
+  { icon: "devicon-svelte-plain", description: "Svelte.js" },
+  { icon: "devicon-nodejs-plain", description: "Node.js" },
+  { icon: "devicon-express-original", description: "Express.js" },
+  { icon: "devicon-firebase-plain", description: "Firebase" },
   { icon: "devicon-mongodb-plain", description: "MongoDB" },
   { icon: "devicon-redux-original", description: "Redux" },
 ];
