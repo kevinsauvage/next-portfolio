73d3985b0d59c528b84443f0267d905d7cfaf9b9
Kevin Sauvage <kevinsauvage@outlook.com>
2023-04-05 23:41:19 +0200

    fix svg

--- src/layout/PageLayout/PageLayout.jsx
+++ src/layout/PageLayout/PageLayout.jsx
@@ -16,23 +16,24 @@ const PageLayout = ({ children, title }) => {
         <title>{titleSeo}</title>
       </Head>
       <Header />
-      <Container>{children}</Container>
+      <Container className={styles.container}>{children}</Container>
       <Footer />
-      <svg viewBox="0 0 1 1">
-        <defs>
-          <linearGradient
-            id="my-cool-gradient"
-            x1="0%"
-            y1="0%"
-            x2="100%"
-            y2="0%"
-            gradientTransform="rotate(60)"
-          >
-            <stop offset="0%" stop-color="var(--primary)" />
-            <stop offset="100%" stop-color="var(--secondary)" />
-          </linearGradient>
-        </defs>
-        <rect x="0" y="0" width="1" height="1" fill="url(#my-cool-gradient)" />
+      <svg
+        style={{ height: '0', position: 'absolute', width: '0' }}
+        aria-hidden="true"
+        focusable="false"
+      >
+        <linearGradient
+          id="my-cool-gradient"
+          x1="0%"
+          y1="0%"
+          x2="100%"
+          y2="0%"
+          gradientTransform="rotate(60)"
+        >
+          <stop offset="0%" stop-color="var(--primary)" />
+          <stop offset="100%" stop-color="var(--secondary)" />
+        </linearGradient>
       </svg>
     </div>
   );
