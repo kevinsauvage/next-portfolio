81fc64bde51d7746909f6be4a51f3af51d56cf47
Kevin Sauvage <kevinsauvage@outlook.com>
2023-04-06 18:33:21 +0200

    update colors

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -2,33 +2,28 @@
 import Image from 'next/image';
 
 import portrait from '../../images/banner.svg';
-import SlideUpAndFadeIn from '../SlideUpAndFadeIn/SlideUpAndFadeIn';
 
 import styles from './Banner.module.scss';
 
 const Banner = () => (
   <section className={styles.Banner}>
     <main className={styles.main}>
-      <SlideUpAndFadeIn>
-        <small>Hello, I'm</small>
-        <h1>Kévin Sauvage.</h1>
-        <h2> I'm a developer</h2>
-        <p className={styles.subtitle}>
-          Crafting unique visual experiences that inspire, communicate, and delight.
-        </p>
-      </SlideUpAndFadeIn>
+      <small>Hello, I'm</small>
+      <h1>Kévin Sauvage.</h1>
+      <h2> I'm a developer</h2>
+      <p className={styles.subtitle}>
+        Crafting unique visual experiences that inspire, communicate, and delight.
+      </p>
     </main>
     <aside className={styles.aside}>
-      <SlideUpAndFadeIn>
-        <Image
-          width="851"
-          height="741"
-          layout="responsive"
-          src={portrait.src}
-          alt="illustration"
-          priority
-        />
-      </SlideUpAndFadeIn>
+      <Image
+        width="851"
+        height="741"
+        layout="responsive"
+        src={portrait.src}
+        alt="illustration"
+        priority
+      />
     </aside>
   </section>
 );
