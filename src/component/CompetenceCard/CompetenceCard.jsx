50912dc0afa164e6d13a72b8f62ba93a6f98f68d
Kevin Sauvage <kevinsauvage@outlook.com>
2023-04-09 20:50:06 +0200

    update compentence section

--- src/component/CompetenceCard/CompetenceCard.jsx
+++ src/component/CompetenceCard/CompetenceCard.jsx
@@ -1,17 +1,12 @@
-import competences from '../../data/competences';
 import SlideUpAndFadeIn from '../SlideUpAndFadeIn/SlideUpAndFadeIn';
 
 import styles from './CompetenceCard.module.scss';
 
-const CompetenceCard = () => (
-  <SlideUpAndFadeIn className={styles.CompetenceCard}>
-    <div className={styles.items}>
-      {competences.map((item) => (
-        <div className={styles.item} key={item.description}>
-          <i className={item.icon} />
-          <p>{item.description}</p>
-        </div>
-      ))}
+const CompetenceCard = ({ item }) => (
+  <SlideUpAndFadeIn>
+    <div className={styles.item}>
+      <i className={item?.icon} />
+      <p>{item?.description}</p>
     </div>
   </SlideUpAndFadeIn>
 );
--- src/component/What/What.jsx
+++ src/component/What/What.jsx
@@ -1,13 +1,20 @@
+import competences from '../../data/competences';
 import CompetenceCard from '../CompetenceCard/CompetenceCard';
 import Section from '../Section/Section';
 import Title from '../Title/Title';
 import UpTitle from '../UpTitle/UpTitle';
 
+import styles from './What.module.scss';
+
 const What = () => (
   <Section id="skills">
     <UpTitle text="FEATURES" />
     <Title>Skills</Title>
-    <CompetenceCard />
+    <div className={styles.items}>
+      {competences.map((item) => (
+        <CompetenceCard item={item} key={item.description} />
+      ))}
+    </div>
   </Section>
 );
 
