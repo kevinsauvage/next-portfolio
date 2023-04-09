126598a75f1cefb273bdc97fecc4fdf0a1e8d1c9
Kevin Sauvage <kevinsauvage@outlook.com>
2023-04-09 19:54:31 +0200

    switch to rem

--- src/component/What/What.jsx
+++ src/component/What/What.jsx
@@ -3,15 +3,11 @@ import Section from '../Section/Section';
 import Title from '../Title/Title';
 import UpTitle from '../UpTitle/UpTitle';
 
-import styles from './What.module.scss';
-
 const What = () => (
   <Section id="skills">
     <UpTitle text="FEATURES" />
     <Title>Skills</Title>
-    <div className={styles.cards}>
-      <CompetenceCard />
-    </div>
+    <CompetenceCard />
   </Section>
 );
 
