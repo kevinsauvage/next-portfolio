172fbbc2bda7dda951c25c8facdba5a277452d68
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-04-19 21:38:56 +0200

    update banner

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,33 +1,43 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import portrait from '../../images/banner.svg';
+import Button from '../Button/Button';
 
 import styles from './Banner.module.scss';
 
-const Banner = () => (
-  <section className={styles.Banner}>
-    <div className={styles.main}>
-      <small>Hello, I'm</small>
-      <h1>Kévin Sauvage.</h1>
-      <h2> I'm a developer</h2>
-      <p className={styles.subtitle}>
-        Crafting unique visual experiences
-        <br /> that inspire, communicate,
-        <br /> and delight.
-      </p>
-    </div>
-    <aside className={styles.aside}>
-      <Image
-        width="851"
-        height="741"
-        layout="responsive"
-        src={portrait.src}
-        alt="banner illustration of a man and a computer"
-        priority
-      />
-    </aside>
-  </section>
-);
+const Banner = () => {
+  const { push } = useRouter();
+  return (
+    <section className={styles.Banner}>
+      <div className={styles.main}>
+        <small>
+          {'< '}Greetings, I'm {'/>'}
+        </small>
+        <h1>Kévin Sauvage</h1>
+        <h2>Expert Developer.</h2>
+        <p className={styles.subtitle}>
+          I specialize in creating distinctive and captivating visual experiences
+          <br /> that spark imagination, convey information,
+          <br /> and evoke emotion.
+        </p>
+        <div className={styles.button}>
+          <Button label="Hire me" onClick={() => push('/#contact')} />
+        </div>
+      </div>
+      <aside className={styles.aside}>
+        <Image
+          width="851"
+          height="741"
+          layout="responsive"
+          src={portrait.src}
+          alt="banner illustration of a man and a computer"
+          priority
+        />
+      </aside>
+    </section>
+  );
+};
 
 export default Banner;
