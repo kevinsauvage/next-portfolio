84cf1859ca2992a30b1e79a0f7f9eb50634c5428
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-04-19 21:09:56 +0200

    fix crossorigin

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -37,7 +37,7 @@ const Document = () => (
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap"
         rel="stylesheet"
