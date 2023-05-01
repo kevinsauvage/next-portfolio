6576e76b74a4d703844b8b81f2d4ecf4e9addd9a
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-05-01 20:58:43 +0200

    update favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -7,8 +7,6 @@ const Document = () => (
         name="description"
         content="Welcome to my portfolio! I'm Kévin Sauvage, a passionate full-stack web developer with expertise in Javascript, React.js, Next.js, Svelte.js, Node.js, and Express.js. With a certification in full-stack web development from Le Wagon and professional experience at Subforce and Extia, I pride myself on delivering efficient, reliable, and visually appealing applications. Let's work together to create something amazing. Explore my portfolio and get in touch today"
       />
-      <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
       <meta property="og:url" content="https://www.kevin-sauvage.com/" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Kévin Sauvage Portfolio" />
@@ -32,10 +30,8 @@ const Document = () => (
         name="twitter:image"
         content="https://res.cloudinary.com/kevincloudname/image/upload/v1649841116/portfolio/K%C3%A9vin-Sauvage-Portfolio_bthtru.png"
       />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link
