1d14396dfaf3fc8d97eb222cac7fc9ed97caffae
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-06-11 21:16:52 +0200

    add canonical

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -7,6 +7,7 @@ const Document = () => (
         name="description"
         content="Looking for a reliable full-stack web developer? Check out Kévin Sauvage's portfolio featuring expert-level skills in Javascript, React.js, and Next.js. Let's create something amazing together"
       />
+
       <meta name="google-site-verification" content="oklUq91tpHdbyYCySQE3l3LtgKkzC-WA8pgnt0vqlbc" />
       <meta property="og:url" content="https://www.kevin-sauvage.com/" />
       <meta property="og:type" content="website" />
@@ -31,6 +32,8 @@ const Document = () => (
         name="twitter:image"
         content="https://res.cloudinary.com/kevincloudname/image/upload/v1649841116/portfolio/K%C3%A9vin-Sauvage-Portfolio_bthtru.png"
       />
+      <link rel="canonical" href="https://www.kevin-sauvage.com/" />
+
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
