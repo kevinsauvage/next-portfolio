2792dadefe379dab05aeb51b4ad8c850efaeb68f
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-06-11 21:46:50 +0200

    add  security

--- next.config.js
+++ next.config.js
@@ -19,6 +19,10 @@ const nextConfig = {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload',
           },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
         ],
         source: '/(.*)',
       },
