c6c20d5af6216e6fe7cd03b07a75385fc1ec10bc
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-06-25 22:03:39 +0200

    add analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,17 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import useSmoothScroll from '../hooks/useSmoothScroll';
 
 import '../../styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }) => {
   useSmoothScroll();
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 };
 
 export default MyApp;
