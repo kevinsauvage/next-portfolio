d8eebfa66e4511b7d53c3cedfa3ca1e383aa9af3
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-07-14 19:49:56 +0200

    fix

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -2,22 +2,31 @@ import Link from 'next/link';
 
 import ContactInfo from '@/components/ContactInfo/ContactInfo';
 import Logo from '@/components/Logo/Logo';
+import { useGlobalContext } from '@/contexts/GlobalContext';
 
 import styles from './Aside.module.scss';
 
-const Aside = () => (
-  <aside className={styles.aside}>
-    <Logo />
-    <p className={styles.text}>
-      I&apos;m Kévin Sauvage, a passionate front-end developer with 2 years of experience. I
-      specialize in creating captivating user experiences and delivering exceptional web solutions.
-    </p>
-    <p className={styles.copyright}>&copy; Kévin Sauvage - {new Date().getFullYear()}</p>
-    <ContactInfo />
-    <div className={styles.button}>
-      <Link href="/#Contact">Hire me !</Link>
-    </div>
-  </aside>
-);
+const Aside = () => {
+  const states = useGlobalContext();
+  return (
+    <aside className={styles.aside}>
+      <Logo />
+      <p className={styles.text}>
+        I&apos;m Kévin Sauvage, a passionate front-end developer with 2 years of experience. I
+        specialize in creating captivating user experiences and delivering exceptional web
+        solutions.
+      </p>
+      <p className={styles.copyright}>&copy; Kévin Sauvage - {new Date().getFullYear()}</p>
+      <ContactInfo />
+      <button
+        className={styles.button}
+        type="button"
+        onClick={() => states.scrollToSection('Contact Me')}
+      >
+        Hire me !
+      </button>
+    </aside>
+  );
+};
 
 export default Aside;
