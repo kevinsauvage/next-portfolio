0b8559aea5c6654e04f6e5e9bdedec405f419db5
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-07-18 10:59:25 +0200

    update theme switcher

--- src/components/PageLayout/PageLayout.jsx
+++ src/components/PageLayout/PageLayout.jsx
@@ -4,6 +4,7 @@ import Aside from '@/components/Aside/Aside';
 import Container from '@/components/Container/Container';
 import Header from '@/components/Header/Header';
 import { GlobalProvider } from '@/contexts/GlobalContext';
+import { ThemeProvider } from '@/contexts/ThemeContext';
 
 import styles from './PageLayout.module.scss';
 
@@ -13,17 +14,18 @@ const PageLayout = ({ children, title }) => {
 
   return (
     <GlobalProvider>
-      <div className={styles.PageLayout}>
-        <Head>
-          <title>{titleSeo}</title>
-        </Head>
-
-        <Container classname={styles.container}>
-          <Aside />
-          <div className={styles.children}>{children}</div>
-          <Header />
-        </Container>
-      </div>
+      <ThemeProvider>
+        <div className={styles.PageLayout}>
+          <Head>
+            <title>{titleSeo}</title>
+          </Head>
+          <Container classname={styles.container}>
+            <Aside />
+            <div className={styles.children}>{children}</div>
+            <Header />
+          </Container>
+        </div>
+      </ThemeProvider>
     </GlobalProvider>
   );
 };
--- src/components/ThemeSwitcher/ThemeSwitcher.js
+++ src/components/ThemeSwitcher/ThemeSwitcher.js
@@ -1,34 +1,11 @@
-import { useEffect, useState } from 'react';
-
+import { useThemeContext } from '@/contexts/ThemeContext';
 import IconMoonOutline from '@/svg/IconMoonOutline';
 import IconSun from '@/svg/IconSun';
 
 import styles from './ThemeSwitcher.module.scss';
 
-const THEME_LIGHT = 'theme-light';
-
 const ThemeSwitcher = () => {
-  const [isDarkTheme, setIsDarkTheme] = useState(true);
-
-  const setLightTheme = () => {
-    const htmlElement = document.querySelector('html');
-    htmlElement.classList.add(THEME_LIGHT);
-    setIsDarkTheme(false);
-  };
-
-  const setDarkTheme = () => {
-    const htmlElement = document.querySelector('html');
-    htmlElement.classList.remove(THEME_LIGHT);
-    setIsDarkTheme(true);
-  };
-
-  useEffect(() => {
-    const htmlElement = document.querySelector('html');
-
-    if (htmlElement.classList.contains(THEME_LIGHT)) {
-      setLightTheme();
-    }
-  }, []);
+  const { setDarkTheme, setLightTheme, isDarkTheme } = useThemeContext();
 
   return (
     <button
