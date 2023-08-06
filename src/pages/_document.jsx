fc2ad76112d4067267dbbc59f5e821fb23375c59
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-08-06 13:17:52 +0200

    fix: update style

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -39,7 +39,7 @@ const Document = () => (
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;300;400;500;600&family=Julius+Sans+One&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto+Mono:wght@200;300;400;500&display=swap"
         rel="stylesheet"
       />
     </Head>
