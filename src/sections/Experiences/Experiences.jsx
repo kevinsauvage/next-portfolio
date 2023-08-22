5f9a3cbb53c7ed318fb0cd340bf52133453d1eb1
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-08-22 20:16:17 +0200

    feat: add cv download button

--- src/sections/Experiences/Experiences.jsx
+++ src/sections/Experiences/Experiences.jsx
@@ -1,15 +1,30 @@
+import Link from 'next/link';
+
 import experiences from '@/config/experiences';
+import IconArrowUpRight from '@/svg/IconArrowUpRight';
 
 import Experience from './Experience/Experience';
 
 import styles from './Experiences.module.scss';
 
 const Experiences = () => (
-  <ul className={styles.list}>
-    {experiences.map((experience) => (
-      <Experience key={experience.period} experience={experience} />
-    ))}
-  </ul>
+  <div className={styles.experiences}>
+    <ul className={styles.list}>
+      {experiences.map((experience) => (
+        <Experience key={experience.period} experience={experience} />
+      ))}
+    </ul>
+    <Link
+      href="/kevin-sauvage-cv.pdf"
+      className={styles.link}
+      aria-label="Download my curriculum"
+      target="_blank"
+      prefetch={false}
+    >
+      View Full Résumé
+      <IconArrowUpRight />
+    </Link>
+  </div>
 );
 
 export default Experiences;
