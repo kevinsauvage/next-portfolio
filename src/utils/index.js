a82a58eeb8a1b4f7359c0ee1fb6136c176d9aad1
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-08-27 18:26:14 +0200

    remove eslitnt rule

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/prefer-default-export
 export const scrollToSection = (sectionLabel) => {
   const sectionId = `#${sectionLabel.split(' ').join('-')}`;
   const sectionElement = document.querySelector(sectionId);
