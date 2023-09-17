189531f5f9e972da5f4001d349bc63fe19e4a144
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-09-17 19:43:44 +0200

    update about

--- src/config/about.js
+++ src/config/about.js
@@ -1,19 +1,19 @@
 const about = [
   {
     content:
-      "Hey there! I'm Kévin Sauvage, and I've spent two awesome years diving into front-end development. I really love making websites super smooth to use. I've learned a bunch of programming languages and tools to make sure I build top-notch digital experiences.",
+      "Hi, I'm Kévin Sauvage, and I've spent the last two years diving into front-end development. I have a real passion for creating user-friendly websites.",
   },
   {
     content:
-      'My trusted companions include Javascript, React.js, Next.js, Svelte.js, Node.js, and Express.js. These are like the secret ingredients that help me whip up captivating and efficient websites that users adore.',
+      "I've learned a bunch of programming languages and tools to make sure I build great websites. I'm comfortable with JavaScript, TypeSript, React.js, Next.js, Svelte.js, Node.js, and Express.js. These are like my secret ingredients for making websites that people love to use.",
   },
   {
     content:
-      "But here's the twist that makes me unique: I made a daring career switch to become a programmer. This shift has given me a fresh outlook and an unwavering commitment to delivering my best. Staying updated with the latest web trends is my thing, so you can count on me for cutting-edge work.",
+      'What makes me unique? I made a bold career change to become a programmer, which has given me a fresh perspective and a strong commitment to doing my best. Staying updated with the latest web trends is my thing, so you can count on me for cutting-edge work.',
   },
   {
     content:
-      "Outside of tech, I like wandering around Barcelona, going on hikes, and staying active. I'm also really into music, movies, and trying out new places to eat. So, that's a bit about me! Let's make something awesome together.",
+      "Outside of tech, I enjoy exploring Barcelona, going on hikes, and staying active. I'm also really into music, movies, and trying out new places to eat.",
   },
 ];
 
