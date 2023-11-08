3c0e6333b262826abbc351aa0330c9d57e55e6a1
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2023-11-08 21:41:32 +0100

    update title

--- src/sections/Banner/Banner.jsx
+++ src/sections/Banner/Banner.jsx
@@ -5,12 +5,7 @@ import styles from './Banner.module.scss';
 const Banner = () => (
   <div className={styles.Banner}>
     <Animation initial={{ opacity: 0, y: '100px' }} animate={{ opacity: 1, y: '0px' }} tag="div">
-      <h1>
-        Fueled by <strong>curiosity</strong>,
-        <br />
-        inspired by endless
-        <strong> possibilities</strong>.
-      </h1>
+      <h1>Building the Future, One Line of Code at a Time</h1>
     </Animation>
     <Animation
       initial={{ opacity: 0, y: '100px' }}
