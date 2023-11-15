ed8b44f184adb5013a8c1e7be38417f014a2c980
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2023-11-15 17:01:31 +0100

    try fix robot.txt

--- src/app/robots.jsx
+++ src/app/robots.jsx
@@ -2,8 +2,6 @@ export default function robots() {
   return {
     rules: {
       allow: '/',
-      crawlDelay: 86_400, // 60*60*24
-      disallow: '',
       userAgent: '*',
     },
     sitemap: 'https://www.kevin-sauvage.com/sitemap.xml',
