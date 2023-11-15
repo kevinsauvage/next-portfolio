8daba1ce0a2257de8eb5f7e4a52b97eab2cc0d90
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2023-11-15 23:29:23 +0100

    improve header animation

--- src/components/Header/HeaderPresenter.jsx
+++ src/components/Header/HeaderPresenter.jsx
@@ -6,24 +6,30 @@ import Header from './Header';
 
 const HeaderPresenter = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrollingUp, setIsScrollingUp] = useState(false);
 
   useEffect(() => {
+    let scrollTopPrevious = 0;
+
     const handleScroll = () => {
-      if (window.scrollY > 30) {
+      const scrollTop = window.pageYOffset;
+      if (scrollTop > 0) {
         setIsScrolled(true);
       } else {
         setIsScrolled(false);
       }
+      if (scrollTop > 0 && scrollTop > scrollTopPrevious) {
+        setIsScrollingUp(false);
+      } else {
+        setIsScrollingUp(true);
+      }
+      scrollTopPrevious = scrollTop;
     };
-
     window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  return <Header isScrolled={isScrolled} />;
+  return <Header isScrolled={isScrolled && isScrollingUp} />;
 };
 
 export default HeaderPresenter;
