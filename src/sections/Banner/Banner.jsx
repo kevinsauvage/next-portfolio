dd5646947d4d99b83d6a3c0acdb7eb780f3a77f6
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2023-11-15 23:31:17 +0100

    change header subtitle

--- src/sections/Banner/Banner.jsx
+++ src/sections/Banner/Banner.jsx
@@ -10,9 +10,8 @@ const Banner = () => (
         <br /> One Line of Code at a Time
       </h1>
       <p className={styles.subtitle}>
-        I thrive on the thrill of solving complex challenges through the power of code. As a
-        dedicated developer, I take pride in transforming visionary concepts into tangible digital
-        solutions. Every problem is an opportunity, and I&apos;m here to craft innovative,
+        As a dedicated developer, I take pride in transforming visionary concepts into tangible
+        digital solutions. Every problem is an opportunity, and I&apos;m here to craft innovative,
         code-driven answers that make a difference
       </p>
     </div>
