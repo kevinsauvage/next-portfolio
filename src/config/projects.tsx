247f84b748db405225f75d8712ab087f67444dad
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2023-12-23 16:36:31 +0100

    feat: update projects

--- src/config/projects.tsx
+++ src/config/projects.tsx
@@ -1,128 +1,58 @@
-const projects = [
-  {
-    description:
-      'Showcase of Spotify API integration: User data, library, and music charts for a personalized experience.',
-    githubLink: ['https://github.com/kevinsauvage/spotify-typescript'],
-    images: {
-      gallery: [
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1696360147/Spotify-profile_2_h8jape.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200,ar_16:9/v1696360147/Spotify-profile_2_h8jape.png',
-        },
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1696360363/Spotify-profile_3_wtzilc.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1696360363/Spotify-profile_3_wtzilc.png',
-        },
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1696360530/Spotify-profile_4_ejxlxa.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1696360530/Spotify-profile_4_ejxlxa.png',
-        },
-      ],
-      thumbnail:
-        'https://res.cloudinary.com/kevincloudname/image/upload/w_500,ar_16:9/v1696360147/Spotify-profile_2_h8jape.png',
-    },
-    languages: 'NextJs Typescript SASS',
-    title: 'Spotify Profile',
-    websiteLink: 'https://spotify-typescript.vercel.app/',
+const SpotifyProfile = {
+  description:
+    'Showcase of Spotify API integration: User data, library, and music charts for a personalized experience.',
+  githubLink: ['https://github.com/kevinsauvage/spotify-typescript'],
+  images: {
+    thumbnail:
+      'https://res.cloudinary.com/kevincloudname/image/upload/w_500,ar_16:9/v1696360147/Spotify-profile_2_h8jape.png',
   },
-  {
-    description:
-      'Your personalized entertainment hub. Discover new titles, watch trailers, and engage in community conversations.',
-    githubLink: [
-      'https://github.com/kevinsauvage/streamyV2-client',
-      'https://github.com/kevinsauvage/streamyV2-server',
-    ],
-    images: {
-      gallery: [
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1691688139/portfolio/Streamy_pj4sqb.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1691688139/portfolio/Streamy_pj4sqb.png',
-        },
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1681756351/portfolio/Streamy/acordeon_streamy.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1681756351/portfolio/Streamy/acordeon_streamy.png',
-        },
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1681756274/portfolio/Streamy/slug_streamy.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1681756274/portfolio/Streamy/slug_streamy.png',
-        },
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1681755433/portfolio/Streamy/filters_streamy.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1681755433/portfolio/Streamy/filters_streamy.png',
-        },
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1681756704/portfolio/Streamy/trailer_streamy.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1681756704/portfolio/Streamy/trailer_streamy.png',
-        },
-      ],
-      thumbnail:
-        'https://res.cloudinary.com/kevincloudname/image/upload/w_500/v1691688139/portfolio/Streamy_pj4sqb.png',
-    },
-    languages: 'ReactJs SASS ExpressJs Mongoose',
-    title: 'Streamy',
-    websiteLink: 'https://streamy.fun/',
+  languages: 'NextJs Typescript SASS',
+  title: 'Spotify Profile',
+  websiteLink: 'https://spotify-typescript.vercel.app/',
+};
+
+const Streamy = {
+  description:
+    'Your personalized entertainment hub. Discover new titles, watch trailers, and engage in community conversations.',
+  githubLink: [
+    'https://github.com/kevinsauvage/streamyV2-client',
+    'https://github.com/kevinsauvage/streamyV2-server',
+  ],
+  images: {
+    thumbnail:
+      'https://res.cloudinary.com/kevincloudname/image/upload/w_500/v1691688139/portfolio/Streamy_pj4sqb.png',
   },
-  {
-    description:
-      'Connecting You to Local Restaurants for Effortless Food Delivery. Satisfy Your Cravings Today!',
-    githubLink: ['https://github.com/kevinsauvage/uptogo'],
-    images: {
-      gallery: [
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1692373671/portfolio/UpToGo_o1magc.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1692373671/portfolio/UpToGo_o1magc.png',
-        },
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1681237627/portfolio/uptogo/cart.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1681237627/portfolio/uptogo/cart.png',
-        },
-      ],
-      thumbnail:
-        'https://res.cloudinary.com/kevincloudname/image/upload/w_500/v1692373671/portfolio/UpToGo_o1magc.png',
-    },
-    languages: 'NextJs SASS MongoDB Redux',
-    title: 'UpToGo',
-    websiteLink: 'https://uptogo.vercel.app/',
+  languages: 'ReactJs SASS ExpressJs Mongoose',
+  title: 'Streamy',
+  websiteLink: 'https://streamy.fun/',
+};
+
+const Portfolio = {
+  description:
+    'Explore my portfolio website—a creative showcase of my best work, reflecting artistry and innovation across diverse projects.',
+  githubLink: ['https://github.com/kevinsauvage/next-portfolio'],
+  images: {
+    thumbnail:
+      'https://res.cloudinary.com/kevincloudname/image/upload/w_500/v1699902817/portfolio/Ke%CC%81vin-Sauvage-developer-portfolio-website_1_t9fh6w.png',
   },
-  {
-    description:
-      'Explore my portfolio website—a creative showcase of my best work, reflecting artistry and innovation across diverse projects.',
-    githubLink: ['https://github.com/kevinsauvage/next-portfolio'],
-    images: {
-      gallery: [
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1699902817/portfolio/Ke%CC%81vin-Sauvage-developer-portfolio-website_1_t9fh6w.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1699902817/portfolio/Ke%CC%81vin-Sauvage-developer-portfolio-website_1_t9fh6w.png',
-        },
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1692728617/K%C3%A9vin-Sauvage_2_irljlb.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1692728617/K%C3%A9vin-Sauvage_2_irljlb.png',
-        },
-        {
-          src: 'https://res.cloudinary.com/kevincloudname/image/upload/w_1000,ar_16:9/v1693067679/K%C3%A9vin-Sauvage_3_jpc8i5.png',
-          thumbnail:
-            'https://res.cloudinary.com/kevincloudname/image/upload/w_200/v1693067679/K%C3%A9vin-Sauvage_3_jpc8i5.png',
-        },
-      ],
-      thumbnail:
-        'https://res.cloudinary.com/kevincloudname/image/upload/w_500/v1699902817/portfolio/Ke%CC%81vin-Sauvage-developer-portfolio-website_1_t9fh6w.png',
-    },
-    languages: 'NextJs Typescript SASS',
-    title: 'My Portfolio',
-    websiteLink: 'https://www.kevin-sauvage.com/',
+  languages: 'NextJs Typescript SASS',
+  title: 'My Portfolio',
+  websiteLink: 'https://www.kevin-sauvage.com/',
+};
+
+const Calculator = {
+  description:
+    'A simple calculator app built with Next.js. It allows users to perform basic arithmetic operations and provides a clean and intuitive user interface.',
+  githubLink: ['https://github.com/kevinsauvage/calculator'],
+  images: {
+    thumbnail:
+      'https://res.cloudinary.com/kevincloudname/image/upload/w_500,h_300/v1703344125/portfolio/Create-Next-App_oetdtn.webp',
   },
-];
+  languages: 'NextJs Typescript Tailwind',
+  title: 'Calculator App',
+  websiteLink: 'https://calculator-inky-mu.vercel.app',
+};
+
+const projects = [SpotifyProfile, Streamy, Portfolio, Calculator];
 
 export default projects;
