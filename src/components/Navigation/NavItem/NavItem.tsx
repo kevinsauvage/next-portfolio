2911ca8ac6916b1d3ba1a6ffafdd6128fdf7ee2f
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2023-12-26 21:46:37 +0100

    fix: avoid pushing hash to history

--- src/components/Navigation/NavItem/NavItem.tsx
+++ src/components/Navigation/NavItem/NavItem.tsx
@@ -13,6 +13,7 @@ const NavItem: React.FC<NavItemProperties> = ({ id, label, activeSectionId, icon
   <Link
     href={`#${id}`}
     className={`${styles.NavItem} ${id === activeSectionId ? styles.active : ''}`}
+    replace
   >
     <span className={styles.icon}>{icon}</span>
     <span className={styles.label}>{label}</span>
