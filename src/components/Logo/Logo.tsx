86dd585a2903386081a14204a457ca272e3af58c
Kevin Sauvage Extia <106667569+kevinSauvage-extia@users.noreply.github.com>
2023-12-30 20:17:06 +0100

    fix: improve accessibility

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ import styles from './Logo.module.scss';
 const Logo = () => (
   <div className={`${styles.logo}`}>
     <Link href="/" title="Scroll to top">
-      <b className={styles.logoName}>Kévin Sauvage</b>
+      <strong className={styles.logoName}>Kévin Sauvage</strong>
     </Link>
   </div>
 );
