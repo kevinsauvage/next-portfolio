8abad550734c3928649bd20fd51cfb299bafa08c
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2024-12-09 19:02:19 +0100

    fix: update sitemap generation to remove unnecessary route mapping

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -2,10 +2,10 @@ const BASE_URL = 'https://www.kevin-sauvage.com';
 const LANGUAGES = ['en', 'fr', 'es'];
 
 export default async function sitemap() {
-  return LANGUAGES.flatMap((lang) => {
-    return ['/'].map((route) => ({
+  return LANGUAGES.map((lang) => {
+    return {
       lastModified: new Date().toISOString(),
-      url: `${BASE_URL}/${lang}${route}`,
-    }));
+      url: `${BASE_URL}/${lang}`,
+    };
   });
 }
