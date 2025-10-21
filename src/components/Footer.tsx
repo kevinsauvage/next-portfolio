ed126c444feb932304c8c65a743f47a26b2eec48
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2025-10-21 11:04:51 +0200

    style: add horizontal padding to Footer container for consistent spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import ContactInfo from './ContactInfo';
 const Footer: React.FC = () => {
   return (
     <footer>
-      <div className='container m-auto flex flex-col-reverse justify-center items-center gap-6 py-6 md:flex-row md:justify-between'>
+      <div className='container m-auto flex flex-col-reverse justify-center items-center gap-6 py-6 px-6 md:flex-row md:justify-between'>
         <p className='text-zinc-100 text-lg font-light md:mb-0'>
           © {new Date().getFullYear()} Kevin Sauvage. All rights reserved.
         </p>
