6fd5b4a5f33eb75a03477796a422244d7e3f2688
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2025-10-27 20:36:16 +0100

    feat(config): add image quality settings to Next.js configuration for enhanced optimization

--- next.config.mjs
+++ next.config.mjs
@@ -46,6 +46,7 @@ const nextConfig = {
     minimumCacheTTL: 60,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    qualities: [25, 50, 75, 85, 100],
     remotePatterns: [
       {
         hostname: 'res.cloudinary.com',
