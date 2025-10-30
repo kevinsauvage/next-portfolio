aba0c510a1947e4dd2962bae3c6fc989f59bdb2f
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2025-10-30 19:53:12 +0100

    refactor(not-found): update 404 page layout and typography for improved readability and consistency

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,22 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { BodyLarge, Display } from '@/components/ui/Typography';
+
 const Custom404 = () => {
   return (
     <div className='flex flex-col-reverse justify-center p-4 md:grid md:grid-cols-2 gap-10 items-center min-h-dvh container m-auto lg:p-20'>
       <div className='col-span-1'>
-        <h1 className='text-4xl font-bold font-heading flex flex-col mb-2 text-zinc-300  max-w-5xl sm:text-5xl sm:leading-snug md:text-6xl md:leading-snug xl:text-7xl leading-snug xl:leading-tight'>
-          Oops! Page Not Found
-        </h1>
-        <p className='text-lg font-extralight mb-5 text-zinc-300 max-w-xl md:text-2xl'>
+        <Display className='mb-2 max-w-5xl'>Oops! Page Not Found</Display>
+        <BodyLarge className='mb-5 max-w-xl'>
           The page you&apos;re looking for doesn&apos;t exist. Let&apos;s get you back on track.
-        </p>
+        </BodyLarge>
         <Link
           href='/'
-          className='w-fit flex rounded-md gap-2 items-center justify-center bg-indigo-800 border border-indigo-900 text-zinc-300 px-10 py-4 text-lg 
-               font-semibold hover:bg-indigo-900 hover:border-indigo-900 hover:text-zinc-200 hover:shadow-[0_20px_50px_rgba(55,_48,_163,_0.7)]
-               focus:bg-indigo-800 focus:border-indigo-800 focus:text-zinc-200 
-               active:bg-indigo-800 active:border-indigo-800 active:text-zinc-200'
+          className='w-fit flex rounded-md gap-2 items-center justify-center bg-primary-800 border border-primary-900 text-zinc-300 px-10 py-4 text-lg 
+               font-semibold hover:bg-primary-900 hover:border-primary-900 hover:text-zinc-200 hover:shadow-[0_20px_50px_rgba(14,_165,_233,_0.4)]
+               focus:bg-primary-800 focus:border-primary-800 focus:text-zinc-200 
+               active:bg-primary-800 active:border-primary-800 active:text-zinc-200'
         >
           Go Back Home
         </Link>
