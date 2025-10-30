6fe54f162228e4ff912af58de35a8143565b8b64
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2025-10-30 16:06:54 +0100

    refactor(sitemap): remove language alternates and add change frequency and priority for improved SEO configuration

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -4,13 +4,8 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      alternates: {
-        languages: {
-          en: `${BASE_URL}/en`,
-          es: `${BASE_URL}/es`,
-          fr: `${BASE_URL}/fr`,
-        },
-      },
+      changeFrequency: 'monthly',
+      priority: 1,
       lastModified: new Date(),
       url: BASE_URL,
     },
