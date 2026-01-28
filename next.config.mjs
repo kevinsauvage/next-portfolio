e77ba6c8eb436be1811bbbeedfd852fe8fdfc73c
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2026-01-28 10:20:05 +0100

    chore(next.config): update minimumCacheTTL for static images to 1 year for better caching

--- next.config.mjs
+++ next.config.mjs
@@ -49,7 +49,7 @@ const nextConfig = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 31_536_000, // 1 year for static portfolio images
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     qualities: [25, 50, 75, 85, 100],
